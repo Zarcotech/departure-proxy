4930ee0338e594fa56b0772bb5e3462c01f4b9f5
Zarcotech <zidan.harb2012@gmail.com>
2025-05-14 14:31:33 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,36 +4,21 @@ document.getElementById("toggleBtn").addEventListener("click", function () {
 });
 
 document.getElementById("panic").addEventListener("click", () => {
-    window.location.href = "/panic";
+    window.location.href = "https://google.com";
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    VANTA.NET({
-        el: "#bg",
-        mouseControls: false,
-        touchControls: false,
-        gyroControls: false,
-        minHeight: 200.0,
-        minWidth: 200.0,
-        scale: 1.0,
-        scaleMobile: 1.0,
-        color: 0x1e90ff,
-        backgroundColor: 0x000000
-    });
-});
-
-document.getElementById("gamesTab").addEventListener("click", function () {
-    document.getElementById("games").style.display = "block";
+document.getElementById("appsTab").addEventListener("click", function () {
+    document.getElementById("apps").style.display = "block";
     document.getElementById("content").style.display = "none";
 
     document.getElementById("sidebar").classList.remove("collapsed");
 
-    document.getElementById("games").classList.add("with-sidebar");
+    document.getElementById("apps").classList.add("with-sidebar");
 });
 
 document.getElementById("departTab").addEventListener("click", function () {
     document.getElementById("content").style.display = "block";
-    document.getElementById("games").style.display = "none";
+    document.getElementById("apps").style.display = "none";
 
     document.getElementById("sidebar").classList.remove("collapsed");
 });
