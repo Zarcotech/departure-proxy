7628fab1122bf24f4c2c57f903ab1e49779b7ba2
Zarcotech <zidan.harb2012@gmail.com>
2025-05-15 14:18:45 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,24 +1,13 @@
-document.getElementById("toggleBtn").addEventListener("click", function () {
-    document.getElementById("sidebar").classList.toggle("collapsed");
-    document.getElementById("content").classList.toggle("collapsed");
-});
-
 document.getElementById("panic").addEventListener("click", () => {
-    window.location.href = "https://google.com";
+    window.location = "https://google.com";
 });
 
-document.getElementById("settingsTab").addEventListener("click", function () {
+document.getElementById("settingsTab").addEventListener("click", () => {
     document.getElementById("settings").style.display = "block";
     document.getElementById("content").style.display = "none";
+})
 
-    document.getElementById("sidebar").classList.remove("collapsed");
-
-    document.getElementById("settings").classList.add("with-sidebar");
-});
-
-document.getElementById("departTab").addEventListener("click", function () {
+document.getElementById("homeTab").addEventListener("click", () => {
     document.getElementById("content").style.display = "block";
     document.getElementById("settings").style.display = "none";
-
-    document.getElementById("sidebar").classList.remove("collapsed");
-});
+})
