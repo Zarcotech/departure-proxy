219a7db3c24e7874f0f631cc7504ec1813f6ae57
Zidan Harb <zidan.harb2012@gmail.com>
2025-05-14 20:40:28 -0500

    Customization

--- public/script.js
+++ public/script.js
@@ -7,18 +7,18 @@ document.getElementById("panic").addEventListener("click", () => {
     window.location.href = "https://google.com";
 });
 
-document.getElementById("appsTab").addEventListener("click", function () {
-    document.getElementById("apps").style.display = "block";
+document.getElementById("settingsTab").addEventListener("click", function () {
+    document.getElementById("settings").style.display = "block";
     document.getElementById("content").style.display = "none";
 
     document.getElementById("sidebar").classList.remove("collapsed");
 
-    document.getElementById("apps").classList.add("with-sidebar");
+    document.getElementById("settings").classList.add("with-sidebar");
 });
 
 document.getElementById("departTab").addEventListener("click", function () {
     document.getElementById("content").style.display = "block";
-    document.getElementById("apps").style.display = "none";
+    document.getElementById("settings").style.display = "none";
 
     document.getElementById("sidebar").classList.remove("collapsed");
 });
