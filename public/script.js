c633f5ca7657cf231847b0f2b3cb4057ab306c2a
Zidan Harb <zidan.harb2012@gmail.com>
2025-05-15 21:27:56 -0500

    Update

--- public/script.js
+++ public/script.js
@@ -1,13 +1,13 @@
 document.getElementById("panic").addEventListener("click", () => {
-    window.location = "https://google.com";
+    window.location.replace("https://google.com");
 });
 
 document.getElementById("settingsTab").addEventListener("click", () => {
-    document.getElementById("settings").style.display = "block";
+    document.getElementById("settings").style.display = "inline";
     document.getElementById("content").style.display = "none";
-})
+});
 
 document.getElementById("homeTab").addEventListener("click", () => {
-    document.getElementById("content").style.display = "block";
+    document.getElementById("content").style.display = "flex";
     document.getElementById("settings").style.display = "none";
-})
+});
