67a4c6567d633d770431f4c4824fb9cafe95b3f2
Zidan Harb <zidan.harb2012@gmail.com>
2025-05-16 16:26:30 -0500

    Frontend completion

--- public/script.js
+++ public/script.js
@@ -11,3 +11,62 @@ document.getElementById("homeTab").addEventListener("click", () => {
     document.getElementById("content").style.display = "flex";
     document.getElementById("settings").style.display = "none";
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("proxyTab").addEventListener("click", () => {
+        document.getElementById("proxy").style.display = "inline";
+        document.getElementById("themes").style.display = "none";
+        document.getElementById("credits").style.display = "none";
+    })
+
+    document.getElementById("themesTab").addEventListener("click", () => {
+        document.getElementById("proxy").style.display = "none";
+        document.getElementById("themes").style.display = "block";
+        document.getElementById("credits").style.display = "none";
+    })
+
+    document.getElementById("creditsTab").addEventListener("click", () => {
+        document.getElementById("proxy").style.display = "none";
+        document.getElementById("themes").style.display = "none";
+        document.getElementById("credits").style.display = "inline";
+    })
+
+    document.getElementById("saveBtn").addEventListener("click", showNotiSave);
+})
+
+function showNotiReset() {
+    document.getElementById("notification").style.display = "flex";
+}
+
+function showNotiSave() {
+    document.getElementById("notificationSave").style.display = "flex";
+    document.body.classList.add("noscroll");
+    setTimeout(() => {
+        document.getElementById("notificationSave").style.display = "none";
+        document.body.classList.remove("noscroll");
+    }, 2000);
+}
+
+document.getElementById("close").addEventListener("click", () => {
+    document.getElementById("notification").style.display = "none";
+})
+
+document.getElementById("closeSave").addEventListener("click", () => {
+    document.getElementById("notificationSave").style.display = "none";
+    document.body.classList.remove("noscroll");
+});
+
+function lightTheme(){
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "#20242b";    
+}
+
+const sidebar2 = document.getElementById("settingsSidebar");
+if (sidebar2) {
+    sidebar2.addEventListener("mouseenter", () => {
+        document.body.classList.add("noscroll");
+    });
+    sidebar2.addEventListener("mouseleave", () => {
+        document.body.classList.remove("noscroll");
+    });
+}
